6e2fb4f48c79c33a0eae3475fed5c8736d87cbda
chocomilku <chocomilku@zohomail.com>
2024-02-10 22:07:15 +0800

    feat: implemented basic features

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 import { argv } from "node:process";
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+const site = argv[2];
+
+const fetchSite = async (url: string) => {
+	const response = await axios(url);
+	const body = await response.data;
+	return body;
+};
+
+const parseImg = (body: unknown) => {
+	const $ = cheerio.load(body as string);
+	const h = $("#screenshot-image").attr("src");
+	console.log(h);
+};
+
+const the = async () => {
+	const res = await fetchSite(site);
+	parseImg(res);
+};
+
+the();
